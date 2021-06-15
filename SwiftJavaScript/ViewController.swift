507fdbd3eeccfbae25b46cfd195d27ac3b06a4c4
sohokay94how <wz330221579@yeah.net>
2021-06-15 18:23:14 +0800

    init

---
+++ SwiftJavaScript/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftJavaScript
+//
+//  Created by hengchengfei on 16/2/1.
+//  Copyright © 2016年 chengfeisoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
